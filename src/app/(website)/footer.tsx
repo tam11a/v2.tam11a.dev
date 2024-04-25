2eb9a14ed274983adf5b7a923896237d1c73d0f2
tam11a <ibrahimsadiktamim@gmail.com>
2024-04-25 09:18:08 +0600

    updated: footer max-w

--- src/app/(website)/footer.tsx
+++ src/app/(website)/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
 	return (
-		<footer className="flex flex-col items-center justify-between max-w-7xl mx-auto px-10 py-9 pb-44">
+		<footer className="flex flex-col items-center justify-between max-w-4xl mx-auto px-10 py-9 pb-44">
 			<h3 className="text-2xl tracking-[0.6rem] w-full mb-11">Connect</h3>
 			<div className="grid grid-cols-6 gap-4 w-full mx-auto">
 				<div className=" flex flex-col col-span-6 md:col-span-1 text-3xl gap-4">
