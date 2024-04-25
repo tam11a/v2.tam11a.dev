034ede6b4b26ad0b5d28f1eecc12f8e465fd2e2f
tam11a <ibrahimsadiktamim@gmail.com>
2024-04-25 07:42:07 +0600

    added: header tag

--- src/app/(website)/components/hero.tsx
+++ src/app/(website)/components/hero.tsx
@@ -15,13 +15,13 @@ export default function Hero() {
 			/>
 			<div className="h-12" />
 			<h1 className="text-2xl font-cover-by-your-grace">Ibrahim Sadik Tamim</h1>
-			<p className="text-[6.5rem] leading-[80%] [&>span]:text-gradient">
+			<h2 className="text-[5rem] lg:text-[6.5rem] leading-[80%] [&>span]:text-gradient transition-all duration-75 ease-out">
 				Full-Stack <br />
 				<span>Software</span> <br />
 				<span>Engineer</span> <br />
 				Since <br />
 				<span>2021</span>
-			</p>
+			</h2>
 		</section>
 	);
 }
