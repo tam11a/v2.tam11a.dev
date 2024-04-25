42ea64e98c7b57b0729513a8ddb54e8b302e359f
tam11a <ibrahimsadiktamim@gmail.com>
2024-04-26 02:07:38 +0600

    added: hero animation

--- src/app/(website)/components/hero.tsx
+++ src/app/(website)/components/hero.tsx
@@ -1,8 +1,33 @@
 "use client";
 import Image from "next/image";
 import MagneticComponent from "../magnetic";
+import { useGSAP } from "@gsap/react";
+import SplitType from "split-type";
+import gsap from "gsap";
 
 export default function Hero() {
+	useGSAP(() => {
+		const myText = new SplitType("#hero-description", {
+			split: "words",
+		});
+
+		gsap.fromTo(
+			myText.words,
+			{
+				y: 110,
+				opacity: 0,
+			},
+			{
+				y: 0,
+				opacity: 1,
+				stagger: 0.05,
+				duration: 0.7,
+				delay: 0.2,
+				ease: "back.out(1.7)",
+			}
+		);
+	});
+
 	return (
 		<section
 			id="hero"
@@ -19,7 +44,10 @@ export default function Hero() {
 			</MagneticComponent>
 			<div className="h-12" />
 			<h1 className="text-2xl font-cover-by-your-grace">Ibrahim Sadik Tamim</h1>
-			<h2 className="text-[5rem] lg:text-[6.5rem] leading-[80%] [&>span]:text-gradient transition-all duration-75 ease-out">
+			<h2
+				className="text-[5rem] lg:text-[6.5rem] leading-[80%] child-gradient [&>span>div]:text-gradient transition-all duration-75 ease-out [&>div>div]:clip-title [&>*]:clip-title [&>div>span>div>div]:opacity-0"
+				id="hero-description"
+			>
 				Full-Stack <br />
 				<span>Software</span> <br />
 				<span>Engineer</span> <br />
