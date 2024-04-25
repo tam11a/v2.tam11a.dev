8b8dbc0809e1352453c1fb07057b0f1ae709a805
tam11a <ibrahimsadiktamim@gmail.com>
2024-04-25 12:24:03 +0600

    updated: cursor movement

--- src/app/(website)/cursor.tsx
+++ src/app/(website)/cursor.tsx
@@ -15,6 +15,8 @@ export default function Cursor() {
 					y: y + 3,
 					height: 80,
 					width: 80,
+					xPercent: -50,
+					yPercent: -50,
 					duration: 0.7,
 					opacity: 1,
 					ease: "sine.out(1, 0.3)",
@@ -40,6 +42,6 @@ export default function Cursor() {
 	}, []);
 
 	return (
-		<div className="cursor fixed top-0 left-0 mix-blend-difference bg-slate-300 -translate-x-1/2 -translate-y-1/2 transform h-20 w-20 opacity-0 pointer-events-none select-none rounded-full z-[10000]" />
+		<div className="cursor fixed top-0 left-0 mix-blend-difference bg-slate-300 opacity-0 pointer-events-none select-none rounded-full z-[10000]" />
 	);
 }
